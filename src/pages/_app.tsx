08b7bc93758cf7fa2bb8eb947171220035396104
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-02-22 14:58:38 -0500

    removes provider wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+// import { UserProvider } from "@auth0/nextjs-auth0/client";
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
+  return <Component {...pageProps} />;
 }
