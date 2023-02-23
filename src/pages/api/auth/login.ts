36a254ff677e149ecfd3e3f5e2f16ed4a12674b4
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-02-23 10:00:35 -0500

    adds mental comment on situation

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,4 +1,8 @@
 // pages/api/auth/[...auth0].js
+
+// This just is not going to work at all as this portion of the library depends on the node runtime.
+// Will investigate authenticating in a different way (manually?) and using the session here
+
 import { handleAuth } from "@auth0/nextjs-auth0";
 
 export const config = { runtime: "experimental-edge" };
