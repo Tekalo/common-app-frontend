cba875cabe9f4a4ce634d81ae3da434e2ed47fdf
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-08 14:03:52 -0500

    feat: creates primary layout component and updates readme

---
+++ src/components/layouts/primary/PrimaryLayout.stories.tsx
@@ -0,0 +1,8 @@
+import PrimaryLayout from './PrimaryLayout';
+import { mockPrimaryLayoutProps } from './PrimaryLayout.mocks';
+
+export default { component: PrimaryLayout };
+
+export const Default = {
+  args: { ...mockPrimaryLayoutProps.base },
+};
