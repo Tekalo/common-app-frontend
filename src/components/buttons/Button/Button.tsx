a3e04643ff8133c6b8618b79eb4e92160f1665b7
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-09 19:21:53 -0500

    feat: centers button text and experiments with router.refresh()

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -17,13 +17,11 @@ const Button: React.FC<IButton> = ({
   className,
 }) => {
   const BASE = `group min-w-[118px] flex h-12 flex-row
-    content-center items-center justify-center gap-[10px] rounded border-2 border-blue-1-primary font-sans text-component-lg font-normal hover:border-blue-2-hover
-    hover:bg-blue-2-hover focus-visible:ring-2 focus-visible:ring-[#A7C4DB] active:border-blue-3-pressed active:bg-blue-3-pressed disabled:border-blue-4-disabled disabled:bg-blue-4-disabled
-  disabled:text-white-text group ${className}`;
+    content-center items-center justify-center rounded font-sans text-component-lg font-normal hover:bg-blue-2-hover focus-visible:ring-2 focus-visible:ring-[#A7C4DB] active:border-blue-3-pressed active:bg-blue-3-pressed disabled:border-blue-4-disabled disabled:bg-blue-4-disabled disabled:text-white-text group ${className}`;
 
   const DEFAULT = `${BASE} bg-blue-1-primary text-white-text`;
 
-  const OUTLINED = `${BASE} bg-white text-blue-1-primary hover:text-white-text`;
+  const OUTLINED = `${BASE} bg-white text-blue-1-primary border-2 border-blue-1-primary hover:border-blue-2-hover hover:text-white-text`;
 
   return (
     <button
--- src/components/navigation/MainNavbar/MainNavbar.tsx
+++ src/components/navigation/MainNavbar/MainNavbar.tsx
@@ -1,43 +1,72 @@
-import { Bars3Icon } from '@heroicons/react/24/outline';
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 import Button from '@/components/buttons/Button/Button';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { SyntheticEvent } from 'react';
 
 export interface IMainNavbar extends React.ComponentPropsWithoutRef<'header'> {}
 
 const MainNavbar: React.FC<IMainNavbar> = ({ className, ...headerProps }) => {
+  const router = useRouter();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  const handleAuthentication = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    isAuthenticated
+      ? logout({ logoutParams: { returnTo: window.location.origin } })
+      : loginWithRedirect();
+
+    router.refresh();
+  };
+
   return (
     <nav className="w-screen bg-white">
-      <div className="mx-auto h-auto max-w-7xl px-20 mobile:px-4 tablet:px-10">
-        <div className="flex justify-between">
+      <div className="mx-auto h-auto max-w-7xl xs:px-1 mobile:px-4 tablet:px-10 desktop:px-20">
+        <div className="space-y-50 flex tablet:justify-between">
           {/* Logo */}
           <div className="flex py-4">
             <Link
               href="/"
-              className="flex flex-row space-x-4 px-8 py-3 text-component-lg text-black-text"
+              className="flex flex-row space-x-4 px-4 py-1 text-component-lg text-black-text tablet:py-3 tablet:px-8"
             >
               LOGO TBD
             </Link>
           </div>
           {/* Primary Nav */}
-          <div className="flex items-center space-x-16 tablet:space-x-2 desktop:space-x-10">
-            <Link
-              href="/"
-              className="hidden px-6 py-3 text-component-xl font-normal text-black-text tablet:block"
+          <div className="flex items-center space-x-6 tablet:space-x-2 desktop:space-x-10">
+            {/* Using regular anchor here for auth0 redirect */}
+            <a
+              className="hidden cursor-pointer px-6 py-3 text-component-xl font-normal text-black-text tablet:block"
+              onClick={(e) => handleAuthentication(e)}
             >
-              Log in
+              {isAuthenticated ? 'Log out' : 'Log in'}
+            </a>
+            <Link
+              href={isAuthenticated ? '/dashboard' : '/sign-up'}
+              className={
+                isAuthenticated
+                  ? 'cursor-pointer px-6 py-3 text-component-xl font-normal text-black-text'
+                  : ''
+              }
+            >
+              {isAuthenticated ? (
+                'My account'
+              ) : (
+                <Button
+                  className="px-4 py-2 font-normal tablet:py-3 tablet:px-8"
+                  label="Get started"
+                  onClick={() => {}}
+                />
+              )}
             </Link>
-            <Link href="/" className="">
-              <Button
-                className="px-4 py-2 font-normal tablet:py-3 tablet:px-8"
-                label="Get started"
-                onClick={() => {}}
-              />
-            </Link>
-          </div>
-          {/* Mobile Button */}
-          <div className="flex items-center tablet:hidden">
-            <Bars3Icon className="h-6 w-6 stroke-2 text-black-text" />
+            {/* Mobile Button */}
+            <div className="flex items-center tablet:hidden">
+              <Bars3Icon className="h-6 w-6 stroke-2 text-black-text" />
+            </div>
           </div>
         </div>
       </div>
