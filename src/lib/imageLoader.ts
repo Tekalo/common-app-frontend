f4f73cdf00a0c9bcce1dca7c54a0811f24fec797
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-13 15:33:00 -0400

    fix: adds cloudflare worker image loader

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,12 +1,20 @@
-const normalizeSrc = (src) => {
+interface ILoader {
+  src: string;
+  width?: number;
+  quality?: number;
+}
+
+const normalizeSrc = (src: string) => {
   return src.startsWith('/') ? src.slice(1) : src;
 };
 
-export const cloudflareLoader = ({ src, width, quality }) => {
+export const cloudflareLoader = ({ src, width, quality }: ILoader) => {
   const params = [`width=${width}`];
+
   if (quality) {
     params.push(`quality=${quality}`);
   }
+
   const paramsString = params.join(',');
   return `https://capp-resizer.smartin.workers.dev/${paramsString}/${normalizeSrc(
     src
