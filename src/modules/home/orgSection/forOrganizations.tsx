725ba7ae818573a2b8794e23db61ec9f3d56687d
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-15 15:16:51 -0400

    feat: finishes org section

--- src/modules/home/orgSection/forOrganizations.tsx
+++ src/modules/home/orgSection/forOrganizations.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/buttons/Button/Button';
 import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
 export interface IForOrganizations {}
@@ -51,18 +52,38 @@ const ForOrganizations: React.FC<IForOrganizations> = () => {
               className="h-16 object-cover"
             />
           </div>
-          <div className="flex flex-row items-center justify-center pt-14">
-            <div className="flex cursor-pointer text-center align-middle text-component-lg text-blue-1-primary transition-all">
-              See More
-              {<ChevronRightIcon className="h-6" />}
-            </div>
+          <div className="space-x-o flex cursor-pointer flex-row items-center justify-center pt-14 text-center align-middle text-component-lg text-blue-1-primary transition-all">
+            See more
+            {<ChevronRightIcon className="h-5 w-5" />}
           </div>
         </div>
       </div>
-      {/* CTA Modal*/}
-      {/* Title */}
-      {/* Content */}
-      {/* Button */}
+      {/* CTA Box*/}
+      <div className="relative flex w-[608px] flex-col items-center pt-28">
+        <div className="z-10 rounded-[10px] bg-[#FFFAF5] ring-4 ring-[#FFBC03]">
+          {/* Content */}
+          <div className="flex flex-col items-center  pt-16 text-center">
+            {/* Title */}
+            <div className="text-displaytext-center text-desktop-h3 text-black-text">
+              <p>Hire candidates that are aligned</p> with your mission.
+            </div>
+            <div className="px-28 pt-6 text-center font-sans text-desktop-md-copy leading-7  text-black-text ">
+              Candidates on [Name TBD] are carefully vetted and screened before
+              you decide to interview
+            </div>
+            {/* Button */}
+            <div className="pt-6 pb-20">
+              <Button
+                label="Apply as an organization"
+                className="py-5 pl-[68px] pr-[71px]"
+                onClick={() => {}}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Offset Blackbox */}
+        <div className="absolute -right-3 -bottom-3 z-0 h-[408px] w-[608px] rounded-2xl bg-[#363D3F]" />
+      </div>
     </section>
   );
 };
