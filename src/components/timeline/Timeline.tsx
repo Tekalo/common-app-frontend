0c237a15fc26a36e56a7d3c08adc24167d994ee9
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-15 11:06:23 -0400

    feat: completes timeline component for about section

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -4,64 +4,30 @@ export interface ITimeline {
   timelineItems: Array<ITimelineItem>;
 }
 
-// TODO: Expect array of ITimelineItem
-
 const Timeline: React.FC<ITimeline> = ({ timelineItems }) => {
   return (
     <div className="px-96 pt-14">
-      <ol className="border-l-2 border-solid border-blue-1-primary">
-        <li>
-          <div className="flex-start flex items-center">
-            <div className="align -ml-[29px] mr-3 h-14 w-14 rounded-full bg-blue-1-primary text-center">
-              1
+      <ol className="-space-y-7">
+        {timelineItems.map((item, i) => (
+          <li
+            key={i}
+            className="border-l-2 border-solid border-blue-1-primary pb-8 last:border-l-0"
+          >
+            <div className="flex-start flex items-center">
+              <div className="-ml-[30px] flex h-14 w-14 items-center justify-center rounded-full bg-blue-1-primary text-center font-display text-component-xl text-white-text">
+                {i + 1}
+              </div>
+              <h4 className="ml-7 mb-1.5 font-display text-desktop-h4 text-black-text">
+                {item.title}
+              </h4>
             </div>
-            <p className="text-sm text-neutral-500 dark:text-neutral-300">
-              01.07.2021
-            </p>
-          </div>
-          <div className="mt-2 ml-4 mb-6">
-            <h4 className="text-xl mb-1.5 font-semibold">Title of section 1</h4>
-            <p className="mb-3 text-neutral-500 dark:text-neutral-300">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              scelerisque diam non nisi semper, et elementum lorem ornare.
-              Maecenas placerat facilisis mollis. Duis sagittis ligula in
-              sodales vehicula.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div className="flex-start flex items-center pt-2">
-            <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
-            <p className="text-sm text-neutral-500 dark:text-neutral-300">
-              13.09.2021
-            </p>
-          </div>
-          <div className="mt-2 ml-4 mb-6">
-            <h4 className="text-xl mb-1.5 font-semibold">Title of section 2</h4>
-            <p className="mb-3 text-neutral-500 dark:text-neutral-300">
-              Libero expedita explicabo eius fugiat quia aspernatur autem
-              laudantium error architecto recusandae natus sapiente sit nam
-              eaque, consectetur porro molestiae ipsam an deleniti.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div className="flex-start flex items-center pt-2">
-            <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
-            <p className="text-sm text-neutral-500 dark:text-neutral-300">
-              25.11.2021
-            </p>
-          </div>
-          <div className="mt-2 ml-4 pb-5">
-            <h4 className="text-xl mb-1.5 font-semibold">Title of section 3</h4>
-            <p className="mb-3 text-neutral-500 dark:text-neutral-300">
-              Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit
-              natus! Eum corporis illum nihil officiis tempore. Excepturi illo
-              natus libero sit doloremque, laborum molestias rerum pariatur quam
-              ipsam necessitatibus incidunt, explicabo.
-            </p>
-          </div>
-        </li>
+            <div className="mt-1 ml-14 mb-6">
+              <p className="mb-3  font-sans text-desktop-md-copy text-black-text">
+                {item.text}
+              </p>
+            </div>
+          </li>
+        ))}
       </ol>
     </div>
   );
--- src/modules/home/howSection/HowItWorks.tsx
+++ src/modules/home/howSection/HowItWorks.tsx
@@ -2,32 +2,47 @@ import Timeline from '@/components/timeline/Timeline';
 import { ITimelineItem } from '@/lib/types';
 import { useState } from 'react';
 
-export interface IHowItWorks {}
+interface IHowItWorks {}
 
-/**
- * Likely will make a reusable component from (https://tailwind-elements.com/docs/standard/components/timeline/#basic)
- *
- * After that this one should call that component and pass a list of items. Each item should have: {label: string, title: string, text: string}
- *
- * The child component will render a new timeline item with each of these passed in
- *
- * This component will also actually render TWO of those components - one for organizations and the other for candidates.
- *
- * // TODO: Create Timeline component (see above)
- * // TODO: Create toggle component (two link nav basically)
- * // TODO: useState for current toggle w/ conditional render
- *
- * Maybe use one timeline component but conditionally pass in different arrays of items? That sounds like a better move...
- */
 const HowItWorks: React.FC<IHowItWorks> = () => {
   const [forOrgs, setForOrgs] = useState(false);
 
   // Store the titems that the component should conditionally render
   const orgItems: Array<ITimelineItem> = [
-    { title: 'Hello Organizations', text: 'Lorem Ipsum' },
+    {
+      title: 'Lorem ipsum dolor',
+      text: 'In sit amet lectus elit. Vivamus nisl eros, egestas eu lorem sed, tempor imperdiet dolor. Etiam fermentum ipsum quis malesuada maximus. Maecenas ut sapien ac mauris porta tincidunt. Vestibulum eleifend blandit dapibus. Proin tempus tincidunt nibh imperdiet tristique. Donec eu dictum magna. Maecenas sed auctor dolor.',
+    },
+    {
+      title: 'Lorem ipsum dolor',
+      text: 'In sit amet lectus elit. Vivamus nisl eros, egestas eu lorem sed, tempor imperdiet dolor. Etiam fermentum ipsum quis malesuada maximus. Maecenas ut sapien ac mauris porta tincidunt. Vestibulum eleifend blandit dapibus. Proin tempus tincidunt nibh imperdiet tristique. Donec eu dictum magna. Maecenas sed auctor dolor.',
+    },
+    {
+      title: 'Lorem ipsum dolor',
+      text: 'In sit amet lectus elit. Vivamus nisl eros, egestas eu lorem sed, tempor imperdiet dolor. Etiam fermentum ipsum quis malesuada maximus. Maecenas ut sapien ac mauris porta tincidunt. Vestibulum eleifend blandit dapibus. Proin tempus tincidunt nibh imperdiet tristique. Donec eu dictum magna. Maecenas sed auctor dolor.',
+    },
+    {
+      title: 'Lorem ipsum dolor',
+      text: 'In sit amet lectus elit. Vivamus nisl eros, egestas eu lorem sed, tempor imperdiet dolor. Etiam fermentum ipsum quis malesuada maximus. Maecenas ut sapien ac mauris porta tincidunt. Vestibulum eleifend blandit dapibus. Proin tempus tincidunt nibh imperdiet tristique. Donec eu dictum magna. Maecenas sed auctor dolor.',
+    },
   ];
   const candidateItems: Array<ITimelineItem> = [
-    { title: 'Hello Candidates', text: 'Lorem Ipsum' },
+    {
+      title: 'Submit a single application',
+      text: 'Your application will be viewed by a matchmaker from the team to be considered for opportunities by over XX,000 employers and organizations. In the application, we can learn more about what causes you’re interested in, and your preferences in the organizations that fit you best.',
+    },
+    {
+      title: 'Receive your matches',
+      text: 'After 3-5 weeks, your assigned matchmaker send you the right opportunities that fit your interests and experience. This is a rolling process and we’ll send you matches until you opt-out or get hired!',
+    },
+    {
+      title: 'Screening and info call',
+      text: 'Connect with your matchmaker for a screening and info call. This is an introductory call that usually takes less than 20 minutes to get on the same page and answer questions.',
+    },
+    {
+      title: 'Connect to your matched organizations',
+      text: 'Tell us the matches that looks good to you, and we’ll connect you.  Your matchmaker will only introduce you to the organizations that you decide you’re interested in.',
+    },
   ];
 
   return (
