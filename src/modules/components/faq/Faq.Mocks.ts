58aeda165ce47cc562e4206713871664b82e161a
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-03-20 22:55:53 -0400

    feat: finishes faq component for now

--- src/modules/components/faq/Faq.Mocks.ts
+++ src/modules/components/faq/Faq.Mocks.ts
@@ -3,12 +3,10 @@ import { IFaq } from './Faq';
 const base: IFaq = {
   faqItems: [
     {
-      toggledOpen: false,
       questionText: 'Hello',
       answerText: 'World',
     },
     {
-      toggledOpen: true,
       questionText: 'Zar',
       answerText: 'Doz',
     },
