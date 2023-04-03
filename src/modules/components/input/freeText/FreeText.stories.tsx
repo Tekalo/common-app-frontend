16250265cc2ef433f688c481d76b095128974200
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-04-03 15:24:05 -0400

    refactor: changes TextInput -> FreeText

---
+++ src/modules/components/input/freeText/FreeText.stories.tsx
@@ -0,0 +1,8 @@
+import FreeText from './FreeText';
+import { mockFreeTextProps } from './FreeText.mocks';
+
+export default { component: FreeText };
+
+export const Default = {
+  args: { ...mockFreeTextProps.base },
+};
---
+++ src/modules/components/input/radioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,8 @@
+import RadioGroup from './RadioGroup';
+import { mockRadioGroupProps } from './RadioGroup.mocks';
+
+export default { component: RadioGroup };
+
+export const Default = {
+  args: { ...mockRadioGroupProps.base },
+};
