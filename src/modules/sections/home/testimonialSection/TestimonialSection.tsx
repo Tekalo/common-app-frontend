91c3056d579efceb0f0848c1d52e1cd908704529
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-04-06 16:49:36 -0400

    refactor: rm old todos and make video section responsive

--- src/modules/sections/home/testimonialSection/TestimonialSection.tsx
+++ src/modules/sections/home/testimonialSection/TestimonialSection.tsx
@@ -1,60 +1,18 @@
-import Testimonial from '@/components/testimonial/Testimonial';
-
 export interface ITestimonialSection {}
 
 const TestimonialSection: React.FC<ITestimonialSection> = () => {
   return (
     <section className="bg-light-orange">
       {/* TITLE */}
-      <div className="pt-20 text-center font-display text-large-caption-desktop uppercase text-gray-1">
-        Tech talent solving impactful problems
+      <div className="m-auto max-w-content-area px-7 pb-10 pt-14 md:px-24 md:pb-14 lg:pb-20 lg:pt-40">
+        <div className="text-center font-display text-large-caption-desktop uppercase text-gray-1 md:text-large-caption-mobile lg:text-large-caption-desktop">
+          Tech talent solving impactful problems lorem ipsum
+        </div>
+
+        <div className="relative m-auto mt-10 max-w-[320px] md:max-w-[428px] lg:max-w-[640px]">
+          <div className="flex h-0 items-center justify-center bg-gray-2 pt-[67%]"></div>
+        </div>
       </div>
-      {/* FIXME: Honestly this approach is bad. I'm going to refactor it.
-      
-      In short, I want to create a single testimonial object that contains the image, quote, attribution and star/swoop icons. 
-
-      Then I want to create totally separate carousel component that takes in an array of react elements and does the state configuration
-
-      TODO: Carousel component that can carousel different objects
-      TODO: Wrap all images (star and swoop icon) in a testimonial 
-      TODO: Plug them into this area 
-
-      TestimonialSection = [
-        {
-          image: "/path/to/src",
-          quote: "Some quote thing",
-          attribution: "So-and-So"
-        }
-      ]
-
-      <Carousel ...props>
-        { TestimonialSection.map((props, i) => (<Testimonial ...props />))}
-      <Carousel />
-      ~~~
-
-      */}
-      {/* STAR ICON */}
-      <div className="pt-1 pl-44">
-        <img
-          src="/images/Star.png"
-          alt="Star"
-          className="h-[107px] w-[107px] object-cover"
-        />
-      </div>
-      {/* TESTIMONIAL */}
-      <div className="h-[337px]">
-        <Testimonial />
-      </div>
-      {/* SWOOP ICON */}
-      <div className="pt-1 pl-[605px]">
-        <img
-          src="/images/Swoop.png"
-          alt="Swoop"
-          className="h-[30px] w-[30px] object-cover"
-        />
-      </div>
-      {/* TODO: CAROUSEL + SLIDER CONTROL */}
-      <div className="pt-16"></div>
     </section>
   );
 };
