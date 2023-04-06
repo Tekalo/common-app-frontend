3eb62c8888f8cecbbc856608fccb074c8d590bae
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-04-06 18:55:29 -0400

    Test/capp 310 cloudflare to api (#45)
    * feat: debugging cloudflare function
    
    * feat: debugging cloudflare function
    
    * feat: further debugging
    
    * feat: correctly sets users locally. trying cloudflare test deployment
    
    * feat: cloudflare is throwing an iterable error
    
    * feat: more debugging
    
    * feat: debugging, narrowed to how params work on edge
    
    * build: breaks from fixed next-on-pages package to latest
    
    * feat: almost debugged... isolating root error
    
    * feat: params aren't zero now, why?
    
    * fix: should work now
    
    * feat: renabled base url in fetch method
    
    * feat: allows function locally or in cloudflare funcrtion
    
    * fix: getting process env correct
    
    * fix: sets production url to dev for now

--- src/pages/api/[[...params]].ts
+++ src/pages/api/[[...params]].ts
@@ -5,36 +5,67 @@ export const config = {
   runtime: 'edge',
 };
 
-const BASE_URL = () => {
+const BASE_URL = (() => {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return 'https://api.example.com';
-    case 'test':
-      return 'https://staging-api.example.com';
-    case 'development':
       return 'https://capp-api.dev.apps.futurestech.cloud';
+    case 'test':
+      return 'https://capp-api.dev.apps.futurestech.cloud';
+    case 'development':
+      return 'http://localhost:3000';
     default:
       return 'http://localhost:3000';
   }
-};
+})();
 
 const fetchResponse = async (req: NextRequest, params: string[]) => {
   const url = `${BASE_URL}/${params.join('/')}`;
-  const response = await fetch(url, {
-    method: req.method,
-    headers: req.headers,
-    body: req.body,
-  });
-
-  const data = await response.json();
-  return data;
+  console.log(url);
+  switch (req.method) {
+    case 'POST':
+      return await fetch(url, {
+        method: 'POST',
+        body: req.body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    case 'PUT':
+      return await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(req.body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    case 'PATCH':
+      return await fetch(url, {
+        method: 'PATCH',
+        body: JSON.stringify(req.body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    case 'DELETE':
+      return await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    default:
+      return await fetch(url, {
+        method: 'GET',
+      });
+  }
 };
 
 export default async function handler(req: NextRequest): Promise<Response> {
+  console.log(process.env.NODE_ENV);
   const params = req.nextUrl.searchParams.getAll('params');
 
   // If params is empty return proxy health
-  if (params.length === 0) {
+  if (params.length === 0 || params[0] === 'undefined') {
     return new Response(
       JSON.stringify({
         proxy: 'OK',
@@ -46,15 +77,16 @@ export default async function handler(req: NextRequest): Promise<Response> {
         },
       }
     );
+  } else {
+    // If params is not empty, pass the request directly the 3rd party API
+    const result = await fetchResponse(req, params);
+    const data = await result.json();
+    // TODO: Fixme if the response is not JSON the worker segfaults
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
-
-  // If params is not empty, pass the request directly the 3rd party API
-  const result = await fetchResponse(req, params);
-
-  return new Response(JSON.stringify(result), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
 }
