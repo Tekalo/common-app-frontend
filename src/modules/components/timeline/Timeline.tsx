9aae56e5bd6bec45b0395cc820d31d156e8c665e
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-04-06 12:52:57 -0400

    refactor: timeline is now responsive

--- src/modules/components/timeline/Timeline.tsx
+++ src/modules/components/timeline/Timeline.tsx
@@ -9,23 +9,25 @@ export interface ITimeline {
 
 const Timeline: React.FC<ITimeline> = ({ timelineItems, horizontal }) => {
   const regularTimeline = (
-    <div className="px-96 pt-14">
-      <ol className="-space-y-7">
+    <div className="mt-10 md:mt-14">
+      <ol className="">
         {timelineItems.map((item, i) => (
-          <li
-            key={i}
-            className="border-l-2 border-solid border-blue-1 pb-8 last:border-l-0"
-          >
-            <div className="flex-start flex items-center">
-              <div className="-ml-[30px] flex h-14 w-14 items-center justify-center rounded-full bg-blue-1 text-center font-display text-component-extra-large text-white">
+          <li key={i} className="relative mb-4 md:mb-6">
+            <div
+              className={`${
+                i === timelineItems.length - 1 ? 'hidden' : ''
+              } absolute -bottom-5 left-5 top-10 border-l-2 border-solid border-blue-1 pb-8 md:-bottom-6 lg:left-7`}
+            ></div>
+            <div className="flex-start flex items-start">
+              <div className="flex h-10 w-10 flex-none items-center justify-center rounded-full bg-blue-1 text-center font-display text-h4-mobile leading-10 text-white lg:h-14 lg:w-14 lg:text-h4-desktop">
                 {i + 1}
               </div>
-              <h4 className="ml-7 mb-1.5 font-display text-h4-desktop text-black-text">
+              <h4 className="ml-4 mt-2 font-display text-h4-mobile text-black-text md:ml-6 lg:text-h4-desktop">
                 {item.title}
               </h4>
             </div>
-            <div className="mt-1 ml-14 mb-6">
-              <p className="mb-3  font-sans text-p2-desktop text-black-text">
+            <div className="ml-14 mt-4 md:ml-16 md:mt-3 lg:ml-20 lg:mt-0">
+              <p className="font-sans text-p2-desktop text-black-text">
                 {item.text}
               </p>
             </div>
@@ -76,7 +78,9 @@ const Timeline: React.FC<ITimeline> = ({ timelineItems, horizontal }) => {
                 </p>
               </div>
             </li>
-            {/* Do not add a separating div if we are the last item. If the next item has a property of isActive=true then change the bg-blue-2 for this div*/}
+            {/* Do not add a separating div if we are the last item. If the
+                next item has a property of isActive=true then change the
+                bg-blue-2 for this div*/}
             {i !== timelineItems.length - 1 ? (
               <div
                 className={`mb-8 h-[2px] w-[124px] rounded-full ${
