966adba163f97ff286f30fbbef7ea55000d84879
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-04-07 09:38:51 -0400

    refactor: add a real video

--- src/modules/sections/home/testimonialSection/TestimonialSection.tsx
+++ src/modules/sections/home/testimonialSection/TestimonialSection.tsx
@@ -10,7 +10,21 @@ const TestimonialSection: React.FC<ITestimonialSection> = () => {
         </div>
 
         <div className="relative m-auto mt-10 max-w-[320px] md:max-w-[428px] lg:max-w-[640px]">
-          <div className="flex h-0 items-center justify-center bg-gray-2 pt-[67%]"></div>
+          <div className="relative h-0 pb-[67%]">
+            {/* TODO: Obviously this is a placeholder video, right now the
+                      aspect ratio is set to 3:2. If we want to change the ratio
+                      when we update the video, all we need to do is change the 
+                      padding-bottom on this element accordingly:
+                      https://faq.dailymotion.com/hc/en-us/articles/360022841393-How-to-preserve-the-player-aspect-ratio-on-a-responsive-page
+             */}
+            <iframe
+              className="absolute left-0 top-0 h-full w-full border-0"
+              src="https://www.youtube.com/embed/Yw6u6YkTgQ4?controls=0"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </section>
