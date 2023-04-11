e458f0ca82f6cc1ae2eeb3e2a3df2260804db065
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-11 17:35:04 -0400

    fix: update sentry config to include environment; update .env.sample to include SENTRY_ENVIRONMENT

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
 
 const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
 
 // Cloudflare requires a different configuration than that recommended
 // in the Sentry nextjs documentation.
@@ -8,4 +9,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN;
 // eslint-disable-next-line no-undef
 export const onRequest: PagesFunction = sentryPlugin({
   dsn: SENTRY_DSN,
+  environment: SENTRY_ENVIRONMENT,
 });
