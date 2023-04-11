72427c68cb916f6720b9a5abf494be1f53928ddd
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-11 10:21:49 -0400

    fix: removed test error page

--- src/pages/api/error.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(
-  _req: NextApiRequest,
-  _res: NextApiResponse
-): Promise<Response> {
-  throw new Error('API throw error test');
-  //   res.status(200).json({ name: 'John Doe' });
-}
