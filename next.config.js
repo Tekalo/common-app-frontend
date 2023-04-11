44c5b0e318b48d1cc8b36eff4734f492773331e1
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-11 14:44:20 -0400

    fix: updated middleware to just include DSN; deleted unneeded configuration files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,42 +10,6 @@ const nextConfig = {
       },
     ];
   },
-
-  // Custom webpack configuration to use your Next.js app with Sentry.
-  // https://nextjs.org/docs/api-reference/next.config.js/introduction
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-  // Optional build-time configuration options
-  // The Sentry config last; otherwise, the source maps the plugin receives may not be final.
-  sentry: {
-    // See the sections below for information on the following options:
-    //   'Configure Source Maps':
-    //     - disableServerWebpackPlugin
-    //     - disableClientWebpackPlugin
-    //     - hideSourceMaps
-    //     - widenClientFileUpload
-    //   'Configure Legacy Browser Support':
-    //     - transpileClientSDK
-    //   'Configure Serverside Auto-instrumentation':
-    //     - autoInstrumentServerFunctions
-    //     - excludeServerRoutes
-    //   'Configure Tunneling to avoid Ad-Blockers':
-    //     - tunnelRoute
-  },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig;
