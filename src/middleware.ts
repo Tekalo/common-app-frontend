65bd5a536c281d987c126bc56f522166b14f4040
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-11 16:15:23 -0400

    fix: disable linting rules for cloudflare PagesFunction type

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ const SENTRY_DSN = process.env.SENTRY_DSN;
 // Cloudflare requires a different configuration than that recommended
 // in the Sentry nextjs documentation.
 // https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
-export const onRequest = sentryPlugin({
+// eslint-disable-next-line no-undef
+export const onRequest: PagesFunction = sentryPlugin({
   dsn:
     SENTRY_DSN ||
     'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
