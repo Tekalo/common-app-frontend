6e83dfbb9a1e908457ab7073c0749b2a2d40e919
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-11 16:58:59 -0400

    fix: don't provide fallback for Sentry DSN config

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN;
 // https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
 // eslint-disable-next-line no-undef
 export const onRequest: PagesFunction = sentryPlugin({
-  dsn:
-    SENTRY_DSN ||
-    'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+  dsn: SENTRY_DSN,
 });
