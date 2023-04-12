7fc5220b6a2673f6733a451ea8cac576cdc1b151
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-04-12 14:26:18 -0400

    feat: updates tailwind and adds button click handler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from 'next/head';
 
 const Home = () => {
+  const handleButtonClick = () => {
+    // Specify the URL of the external page to open
+    const externalUrl = 'https://airtable.com/shrIpkhetnBCJT0y9';
+
+    // Open the external page in a new window or tab
+    window.open(externalUrl, '_blank');
+  };
+
   return (
     <>
       <Head>
@@ -9,13 +17,13 @@ const Home = () => {
       <div
         className={`flex min-h-screen min-w-full flex-col items-center text-center`}
       >
-        <div className="sm:w-[342px] md:w-[462px] lg:w-[544px] w-[320px] text-black-text">
+        <div className="w-[320px] text-black-text sm:w-[342px] md:w-[462px] lg:w-[544px]">
           {/* Logo */}
           <div className="mt-8">
             <img
               src="/images/logos/TekaloLogo.png"
               alt="Logo"
-              className="w-44 mx-auto"
+              className="mx-auto w-44"
             />
           </div>
           {/* Tagline */}
@@ -37,9 +45,9 @@ const Home = () => {
             {' '}
             {/* Add mx-auto class for centering */}
             <button
-              onClick={() => {}}
+              onClick={handleButtonClick}
               className={
-                'h-12 bg-blue-1 font-sans text-component-large hover:bg-blue-2 active:border-blue-3 active:bg-blue-3 disabled:border-blue-4 disabled:bg-blue-4 md:w-[296px] lg:w-[352px] group group flex w-[294px] min-w-[118px] flex-row content-center items-center justify-center rounded text-white transition-colors focus-visible:ring-2 focus-visible:ring-[#A7C4DB] disabled:text-white'
+                'group group flex h-12 w-[294px] min-w-[118px] flex-row content-center items-center justify-center rounded bg-blue-1 font-sans text-component-large text-white transition-colors hover:bg-blue-2 focus-visible:ring-2 focus-visible:ring-[#A7C4DB] active:border-blue-3 active:bg-blue-3 disabled:border-blue-4 disabled:bg-blue-4 disabled:text-white md:w-[296px] lg:w-[352px]'
               }
             >
               <div className="flex items-center justify-center">
