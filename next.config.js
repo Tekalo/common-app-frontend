240c9e902d7144b3f8ccc72ca62db15bf66f4449
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-12 13:53:07 -0400

    fix: attempting to use the browser config to send errors to sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
@@ -18,10 +14,20 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+  hideSourceMaps: true,
+};
 
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { hideSourcemaps: true },
-);
+// This sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import { NextPageWithLayout } from '@/lib/types';
 import '@/styles/globals.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+import * as Sentry from '@sentry/browser';
 import type { AppProps } from 'next/app';
 
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout;
 }
 
+const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_ENVIRONMENT = process.env.ENVIRONMENT;
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    release: process.env.npm_package_version,
+    environment: SENTRY_ENVIRONMENT,
+    integrations: [new Sentry.BrowserTracing()],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
