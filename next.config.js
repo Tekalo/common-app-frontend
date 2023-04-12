abbc49c8535e0b731fcc8c8b456587c7cd96171e
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-12 14:30:11 -0400

    Revert "fix: reset next config"
    This reverts commit a202f57f4b73417c9ccc294ff3eeb784fda9f292.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,4 +14,20 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+  hideSourceMaps: true,
+};
+
+// This sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
