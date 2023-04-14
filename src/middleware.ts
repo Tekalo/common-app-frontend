06c26d102bcb5af62f9d8b0418aa653d24ab2e28
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 18:08:11 -0400

    fix: added runtime edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
 import { NextResponse } from 'next/server';
 
+export const config = { runtime: 'edge' };
+
 // Sentry plugin for capturing server-side errors
 // https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
 // eslint-disable-next-line no-undef
@@ -8,6 +10,6 @@ export const onRequest: PagesFunction = sentryPlugin({
   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
 });
 
-export default function middleware() {
+export default function middleware(): Response {
   return NextResponse.next();
 }
