2fe02a7d5892ea312e3cb14267cc35287b2d4a64
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 18:34:00 -0400

    fix: will async magically fix this?

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export const config = { runtime: 'experimental-edge' };
 //   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
 // });
 
-export default function middleware(): Response {
+export default async function middleware() {
   sentryPlugin({
     dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
   });
