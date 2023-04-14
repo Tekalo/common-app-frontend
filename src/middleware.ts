8327f7bf72d975b6682e6dfbf7a1bf9c34284c64
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 18:26:39 -0400

    fix: adding sentry plugin initialization in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,13 @@ export const config = { runtime: 'experimental-edge' };
 // Sentry plugin for capturing server-side errors
 // https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
 // eslint-disable-next-line no-undef
-export const onRequest: PagesFunction = sentryPlugin({
-  dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
-});
+// export const onRequest: PagesFunction = sentryPlugin({
+//   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+// });
 
 export default function middleware(): Response {
+  sentryPlugin({
+    dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+  });
   return NextResponse.next();
 }
