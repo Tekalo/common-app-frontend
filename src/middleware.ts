2fc0e46220f8992f8a41f096064e85fdc3300dba
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 17:54:49 -0400

    fix: do something in middleware to continue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,13 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
+import { NextResponse } from 'next/server';
 
+// Sentry plugin for capturing server-side errors
+// https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
 // eslint-disable-next-line no-undef
 export const onRequest: PagesFunction = sentryPlugin({
   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
 });
 
-export default function middleware() {}
+export default function middleware() {
+  return NextResponse.next();
+}
