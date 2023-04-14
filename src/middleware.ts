ea7971615006c6ee0c58542a60f9531020e6c646
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 17:38:45 -0400

    fix: add runtime edge:

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
 
+export const config = { runtime: 'edge' };
+
 // eslint-disable-next-line no-undef
 export const onRequest: PagesFunction = sentryPlugin({
   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
