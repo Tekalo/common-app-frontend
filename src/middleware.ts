a793bdbc30846f42e7932dc09013857f07e07561
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-14 17:36:42 -0400

    fix: update code for linter

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import sentryPlugin from '@cloudflare/pages-plugin-sentry';
+
+// eslint-disable-next-line no-undef
+export const onRequest: PagesFunction = sentryPlugin({
+  dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+});
+
+export default function middleware() {}
---
+++ src/pages/api/hello.ts
@@ -0,0 +1,3 @@
+export default function handler() {
+  throw new Error('Unhandled server side error');
+}
