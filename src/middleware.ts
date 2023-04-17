e1aad26dc3b1bfbbf4f03f41d41dd983c5c1d0e4
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-17 12:44:43 -0400

    fix: trying things I already tried

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
-import { NextResponse } from 'next/server';
 
 export const config = {
   runtime: 'experimental-edge',
@@ -14,8 +13,17 @@ export const config = {
 // });
 
 export default async function middleware() {
-  sentryPlugin({
-    dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
-  });
-  return NextResponse.next();
+  // sentryPlugin({
+  //   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+  // });
+  // return NextResponse.next();
 }
+
+// eslint-disable-next-line no-undef
+export const onRequest: PagesFunction<{
+  SENTRY_DSN: string;
+}> = (context) => {
+  return sentryPlugin({
+    dsn: context.env.SENTRY_DSN,
+  })(context);
+};
