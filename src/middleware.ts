d198e1d9def9d2bf0bd407573ae9451ed8899d52
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-17 13:20:40 -0400

    fix: trying yet another way to capture errors on the server

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
-import sentryPlugin from '@cloudflare/pages-plugin-sentry';
+import * as Sentry from '@sentry/react';
+import { NextResponse } from 'next/server';
 
 export const config = {
   runtime: 'experimental-edge',
-  matcher: ['/pages/:path*', '/pages/api/:path*'],
 };
+
+const SENTRY_DSN =
+  'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
 
 // Sentry plugin for capturing server-side errors
 // https://developers.cloudflare.com/pages/platform/functions/plugins/sentry/
@@ -17,13 +25,19 @@ export default async function middleware() {
   //   dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
   // });
   // return NextResponse.next();
+  try {
+    return await NextResponse.next();
+  } catch (thrown) {
+    Sentry.captureException(thrown);
+    throw thrown;
+  }
 }
 
 // eslint-disable-next-line no-undef
-export const onRequest: PagesFunction<{
-  SENTRY_DSN: string;
-}> = (context) => {
-  return sentryPlugin({
-    dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
-  })(context);
-};
+// export const onRequest: PagesFunction<{
+//   SENTRY_DSN: string;
+// }> = (context) => {
+//   return sentryPlugin({
+//     dsn: 'https://957fb85e991e41e1b624969dec7932ef@o4504962952724480.ingest.sentry.io/4504991639928833',
+//   })(context);
+// };
