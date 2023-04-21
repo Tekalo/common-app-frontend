5788547253f2ab5163e6bc4bb8ea84fb5f0c2a5b
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-04-21 15:40:50 -0400

    feat: build org review page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,11 @@
+import {
+  EmploymentType,
+  OrgSize,
+  OrgType,
+  Skills,
+  VisaSponsorship,
+  YearsOfExperience,
+} from '@/lib/enums';
 import { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
 
@@ -64,3 +72,49 @@ export type BasisTableData = {
     basis: string | JSX.Element;
   }[];
 };
+
+// TODO: Check naming
+// ORG REVIEW
+export interface IOpportunity {
+  organization: IOrganization;
+  contact: IContact;
+  submissions: ISubmission[];
+}
+
+export interface IContact {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export interface ISubmission {
+  roleType: string;
+  positionTitle: string;
+  fullyRemote: boolean;
+  location: string;
+  paid: boolean;
+  pitchEssay: string;
+  source: string;
+  employmentType: EmploymentType;
+  salaryRange: string;
+  desiredHoursPerWeek?: string;
+  desiredStartDate?: Date;
+  desiredEndDate?: Date;
+  jdUrl: string;
+  desiredYoe: YearsOfExperience;
+  desiredSkills: Skills[];
+  desiredOtherSkills: string;
+  visaSponsorship: VisaSponsorship;
+  similarStaffed: boolean;
+  desiredImpactExp: string;
+}
+
+export interface IOrganization {
+  name: string;
+  type: OrgType;
+  size: OrgSize;
+  impactAreas: string;
+  eoe: boolean;
+}
+
+// END ORG REVIEW
