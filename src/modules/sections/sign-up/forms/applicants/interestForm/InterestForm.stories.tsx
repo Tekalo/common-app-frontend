e317facab0311544f0f942c26e929d07d36c7648
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-04-24 16:53:15 -0400

    fix: storybook

--- src/modules/sections/sign-up/forms/applicants/interestForm/InterestForm.stories.tsx
+++ src/modules/sections/sign-up/forms/applicants/interestForm/InterestForm.stories.tsx
@@ -1,7 +1,27 @@
-import InterestForm from './InterestForm';
+import { DndProvider } from 'react-dnd';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import InterestForm, { IInterestForm } from './InterestForm';
 import { mockInterestFormProps } from './InterestForm.mocks';
 
-export default { component: InterestForm };
+const DefaultComponent: React.FC<IInterestForm> = ({
+  handleSave,
+  handleSubmit,
+  savedForm,
+}) => {
+  return (
+    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+      <div className="m-auto max-w-[352px]">
+        <InterestForm
+          handleSave={handleSave}
+          handleSubmit={handleSubmit}
+          savedForm={savedForm}
+        />
+      </div>
+    </DndProvider>
+  );
+};
+
+export default { component: DefaultComponent };
 
 export const Default = {
   args: { ...mockInterestFormProps.base },
