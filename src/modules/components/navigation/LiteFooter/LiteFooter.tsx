881fce8c1b5ca4674c044553c2c05b49e454902c
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-05-03 16:56:17 -0400

    feat: update logos, lite navbar and footer

--- src/modules/components/navigation/LiteFooter/LiteFooter.tsx
+++ src/modules/components/navigation/LiteFooter/LiteFooter.tsx
@@ -5,19 +5,23 @@ export interface ILiteFooter {}
 const LiteFooter: React.FC<ILiteFooter> = () => {
   return (
     <section className="w-full bg-gray-4">
-      <div className="px-8 pb-10 pt-8 md:px-20 md:pb-24 md:pt-12 lg:px-0">
-        <div className="m-auto flex max-w-content-area flex-row flex-wrap items-start gap-x-14 gap-y-8 md:items-center md:justify-between">
+      <div className="px-8 pb-10 pt-14 md:px-20 md:pb-24 md:pt-12 lg:px-0">
+        <div className="m-auto flex max-w-content-area flex-col flex-wrap items-start gap-x-14 gap-y-7 md:flex-row md:items-center md:justify-between">
           {/* Logo */}
-          <div className="items-center rounded-lg bg-gray-3 px-16 py-3 text-white">
-            <div>LOGO</div>
+          <div className="">
+            <img
+              src="/images/logo_lite_footer.png"
+              alt="Tekalo Logo"
+              className="max-w-[65px] py-1 md:max-w-[none]"
+            />
           </div>
           {/* Menu Col 1 */}
           <div className="flex flex-col justify-between space-y-4 font-sans text-component-small text-black-text md:flex-row md:space-x-6 md:space-y-0">
-            <div className="flex-none">
-              <Link href="/privacy-info">Privacy Policy</Link>
+            <div className="flex-none lg:underline">
+              <Link href="/privacy-info">Privacy Info</Link>
             </div>
-            <div className="flex-none">
-              <Link href="/terms-and-conditions">Terms of Service</Link>
+            <div className="flex-none lg:underline">
+              <Link href="/terms-and-conditions">Terms of Use</Link>
             </div>
             <div className="flex-none">© All Rights Reserved</div>
           </div>
