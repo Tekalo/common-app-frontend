e13542b6f1a435bb9cd4932ffe3beb65f34f0535
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-05-04 14:41:34 -0400

    fix: mock interface

--- src/modules/sections/sign-up/forms/organizations/reviewForm/ReviewForm.mocks.ts
+++ src/modules/sections/sign-up/forms/organizations/reviewForm/ReviewForm.mocks.ts
@@ -3,6 +3,8 @@ import { IReviewFormPage } from './ReviewForm';
 const base: IReviewFormPage = {
   orgInfo: undefined,
   orgRoles: [],
+  handleGoToOrg: () => void {},
+  handleGoToRole: (idx) => void {},
 };
 
 export const mockReviewFormPageProps = {
