6ac56a545009309915149b06cbc2d259acd1e62b
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-05-09 09:17:17 -0400

    feat: migrates about language and checks for routing

--- src/modules/sections/home/heroSection/HeroSection.tsx
+++ src/modules/sections/home/heroSection/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Button, { ButtonVariant } from '@/components/buttons/Button/Button';
-import { HOME_HERO } from '@/lang/en';
+import { HOME_HERO_TEXT } from '@/lang/en';
 import { APPLICANT_SIGNUP_LINK, ORG_SIGNUP_LINK } from '@/lib/constants/text';
 import router from 'next/router';
 
@@ -28,24 +28,24 @@ const HeroSection: React.FC<IHeroSection> = () => {
             />
             {/* Desktop Hero */}
             <div className="mb-6 text-center font-display text-h2-mobile text-black-text sm:text-h1-mobile md:mb-4 md:text-left md:text-h1-mobile lg:text-h1-desktop">
-              {HOME_HERO.HEADER}
+              {HOME_HERO_TEXT.HEADER}
             </div>
             <div className="mb-6 text-center text-p1-mobile md:mb-8 md:text-left lg:mb-8 lg:text-p1-desktop">
-              {HOME_HERO.BODY}
+              {HOME_HERO_TEXT.BODY}
             </div>
             <div className="mb-6 text-center md:hidden">
-              {HOME_HERO.MOBILE_BODY}
+              {HOME_HERO_TEXT.MOBILE_BODY}
             </div>
             {/* CTA Buttons */}
             <div className="flex justify-center gap-x-1 sm:gap-x-4 md:justify-start md:gap-x-6">
               <Button
-                label={HOME_HERO.APPLICANT_CTA}
+                label={HOME_HERO_TEXT.APPLICANT_CTA}
                 className="flex-none flex-nowrap p-2 sm:px-4 sm:py-3 md:flex-auto md:px-7 md:py-3 lg:flex-none lg:px-10"
                 onClick={() => router.push(APPLICANT_SIGNUP_LINK)}
               />
               <Button
                 variant={ButtonVariant.OUTLINED}
-                label={HOME_HERO.ORG_CTA}
+                label={HOME_HERO_TEXT.ORG_CTA}
                 className="flex-none flex-nowrap p-1 sm:p-3 md:flex-auto lg:flex-none lg:px-7"
                 onClick={() => router.push(ORG_SIGNUP_LINK)}
               />
