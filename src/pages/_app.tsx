257e98a018e6ba1c6863799bd16b3b8b052af83b
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-05-11 14:07:30 -0400

    feat: tries console logging env and setting it in build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,10 +20,11 @@ Sentry.init({
 function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
+  console.log(process.env);
   return (
     // TODO: Move to env variables
     <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
+      domain={process.env.AUTH0_DOMAIN || ''}
       clientId={process.env.AUTH0_CLIENT_ID || ''}
       authorizationParams={{
         audience: process.env.AUTH0_AUDIENCE || '',
