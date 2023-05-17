35fbc1ae27d8fa54cbb92973ef7d39520428f21c
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-05-17 16:46:28 -0400

    feat: make optional, add to develop

--- cypress/e2e/candidate-application.cy.ts
+++ cypress/e2e/candidate-application.cy.ts
@@ -3,6 +3,7 @@ describe('Candidate Application', () => {
     console.log(Cypress.env());
     cy.visit({
       url: '/sign-up/applicants',
+      // TODO: Move this to a session callback so we don't have to do it every time
       headers: {
         'CF-Access-Client-Id': Cypress.env('cf_access_id'),
         'CF-Access-Client-Secret': Cypress.env('cf_access_secret'),
