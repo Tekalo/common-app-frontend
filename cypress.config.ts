93a8cb0fa0aa12bf4b601f0e1c62222c49203713
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-05-17 12:17:13 -0400

    feat: start preview config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "cypress";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({ path: '.env.local' });
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.baseUrl =
+        process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3001';
+      config.env.auth0_username = process.env.NEXT_PUBLIC_TEST_USER;
+      config.env.auth0_password = process.env.NEXT_PUBLIC_TEST_PASSWORD;
+      config.env.auth0_domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
+      config.env.auth0_audience = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE;
+      config.env.auth0_client_id = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
+
+      return config;
     },
   },
 });
---
+++ cypress/e2e/candidate-application.cy.ts
@@ -0,0 +1,7 @@
+describe('Candidate Application', () => {
+  it('passes', () => {
+    cy.visit('/sign-up/applicants');
+
+    expect(true).to.equal(true);
+  });
+});
