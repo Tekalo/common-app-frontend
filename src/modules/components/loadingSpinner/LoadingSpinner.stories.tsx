ec073e78e895b10164f851493ccec9009048e956
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-06-02 12:58:46 -0400

    feat: adds spinner component and redirects to application if submitted

---
+++ src/modules/components/loadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,8 @@
+import LoadingSpinner from './LoadingSpinner';
+import { mockLoadingSpinnerProps } from './loadingSpinner.mocks';
+
+export default { component: LoadingSpinner };
+
+export const Default = {
+  args: { ...mockLoadingSpinnerProps.base },
+};
