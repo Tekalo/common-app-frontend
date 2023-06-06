9405c22f7c128e45d89c856e793465c2ddd0b4c5
tshaneh <129786566+tshaneh@users.noreply.github.com>
2023-06-06 11:27:07 -0400

    fix: social meta tags (#184) (#185) (#186)

--- src/lang/en.tsx
+++ src/lang/en.tsx
@@ -5,10 +5,12 @@ import Link from 'next/link';
 
 // META
 const META = {
+  ALT_TEXT: 'Tekalo: Match to what matters; build a better world.',
   DESCRIPTION:
     'Tekalo is an initiative that matches tech talent with impact-driven organizations. Now more than ever, impact-driven orgs need tech talent that is passionate about solving the world’s most pressing problems.',
   TITLE: 'Tekalo | Do Good',
-  IMAGE: '/images/tekalo_social.png',
+  TWITTER_HANDLE: '@schmidtfutures',
+  IMAGE: `https://www.tekalo.org/images/tekalo_social.png`,
 };
 
 // NAV LINKS
