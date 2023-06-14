dace49b8a4634d8a07f0cd3df74402ede00ea74a
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-06-14 13:48:02 -0700

    Capp 793 logged in spinner visual change (#223)
    * feat: pulls loading spinner to higher level component for candidate sign up
    
    * fix: mocks

--- src/pages/sign-up/applicants/index.tsx
+++ src/pages/sign-up/applicants/index.tsx
@@ -2,6 +2,7 @@ import ErrorModal from '@/components/modal/Modal/ErrorModal/ErrorModal';
 import TableModal from '@/components/modal/Modal/TableModal/TableModal';
 import NavTitle from '@/components/navigation/NavTitle/NavTitle';
 import {
+  ACCOUNT_LINK,
   APPLICANT_CONTENT_TABLE_TEXT,
   APPLICANT_EXPERIENCE_LINK,
   APPLICANT_FORM_TEXT,
@@ -14,6 +15,9 @@ import {
 } from '@/lang/en';
 import {
   applicantsEndpoint,
+  applicantSubmissionsEndpoint,
+  existingApplicantEndpoint,
+  get,
   postWithTurnstile,
 } from '@/lib/helpers/apiHelpers';
 import {
@@ -21,12 +25,17 @@ import {
   stripEmptyFields,
 } from '@/lib/helpers/formHelpers';
 import ApplicationLayout from '@/lib/layouts/application/ApplicationLayout';
-import { NewCandidateType, NextPageWithLayout } from '@/lib/types';
+import {
+  NewCandidateType,
+  NextPageWithLayout,
+  SubmissionResponseType,
+} from '@/lib/types';
+import LoadingSpinner from '@/modules/components/loadingSpinner/LoadingSpinner';
 import ApplicantSignupForm from '@/sections/sign-up/forms/applicants/signupForm/SignupForm';
 import { useAuth0 } from '@auth0/auth0-react';
 import Link from 'next/link';
 import router from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const privacyModalExtras = (
   <div className="text-p3-desktop">
@@ -41,11 +50,64 @@ const privacyModalExtras = (
 );
 
 const ApplicantSignup: NextPageWithLayout = () => {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently, isLoading, user } =
+    useAuth0();
   const [showPrivacyModal, setShowPrivacyModal] = useState(false);
   const [showErrorModal, setShowErrorModal] = useState(false);
   const [isConflict, setIsConflict] = useState(false);
   const [isTurnstileValid, setIsTurnstileValid] = useState<boolean>(true);
+  const [showContent, setShowContent] = useState<boolean>(false);
+
+  /** Get user data on page load */
+  useEffect(() => {
+    // Check if user has  an application
+    const hasSubmitted = async (): Promise<boolean> => {
+      return get(
+        applicantSubmissionsEndpoint,
+        await getAccessTokenSilently()
+      ).then(async (res) => {
+        if (res.ok) {
+          const submissions = (await res.json()) as SubmissionResponseType;
+          return submissions.isFinal;
+        } else {
+          return false;
+        }
+      });
+    };
+
+    // Check if user exists
+    const hasAccountData = async (): Promise<boolean> => {
+      return get(
+        existingApplicantEndpoint,
+        await getAccessTokenSilently()
+      ).then(async (res) => {
+        if (res.ok) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    };
+
+    const redirectUserCheck = async () => {
+      const hasSubmittedApplication = await hasSubmitted();
+      const hasAccount = await hasAccountData();
+
+      if (hasSubmittedApplication) {
+        router.push(ACCOUNT_LINK);
+      } else if (hasAccount) {
+        router.push(APPLICANT_EXPERIENCE_LINK);
+      } else {
+        setShowContent(true);
+      }
+    };
+
+    if (!isLoading && isAuthenticated && user) {
+      redirectUserCheck();
+    } else {
+      setShowContent(true);
+    }
+  }, [isLoading, isAuthenticated, user, getAccessTokenSilently]);
 
   const displayErrorModal = (): void => {
     setShowErrorModal(true);
@@ -97,54 +159,73 @@ const ApplicantSignup: NextPageWithLayout = () => {
   };
 
   return (
-    <div className="flex min-h-screen min-w-full flex-col items-center">
-      <div className="px-6 pb-28 pt-10 md:px-24">
-        <NavTitle
-          title={APPLICANT_FORM_TEXT.HEADER}
-          navawayText={'Already have an account? '}
-          navLink={SIGN_IN_LINK}
-          navText={'Sign in'}
-        />
-        <div className="m-auto mt-8 max-w-[344px] md:mt-10 lg:mt-8">
-          {/* New user form */}
-          <ApplicantSignupForm
-            showUserExistsError={isConflict}
-            handleSubmit={handleSubmit}
-            setShowPrivacyModal={setShowPrivacyModal}
-            isTurnstileValid={isTurnstileValid}
-            setIsTurnstileValid={setIsTurnstileValid}
-          />
+    <div className="flex min-h-screen min-w-full flex-col items-center justify-center">
+      {isLoading ? (
+        <div className="mb-1/2 space-y-3">
+          <LoadingSpinner />
+          <h3 className="text-component-small-desktop text-center ">
+            {'Hang tight'}
+          </h3>
         </div>
-        {/* Navaway for organizations */}
-        <div className="mt-6 text-center">
-          {APPLICANT_FORM_TEXT.IFORG[0]}
-          <span className="text-blue-1 underline underline-offset-4">
-            <Link href={ORG_SIGNUP_LINK}>{APPLICANT_FORM_TEXT.IFORG[1]}</Link>
-          </span>
-        </div>
-      </div>
-      <TableModal
-        tableData={APPLICANT_CONTENT_TABLE_TEXT}
-        headerText={PRIVACY_MODAL_TEXT.HEADER}
-        bodyText={PRIVACY_MODAL_TEXT.BODY}
-        extras={privacyModalExtras}
-        isOpen={showPrivacyModal}
-        closeModal={() => {
-          setShowPrivacyModal(false);
-        }}
-      />
-      <ErrorModal
-        isOpen={showErrorModal}
-        titleText={ERROR_MODAL_TEXT.requestFailed}
-        descriptionText={ERROR_MODAL_TEXT.somethingWrong}
-        buttonText={ERROR_MODAL_TEXT.okButton}
-        buttonHandler={() => {
-          isConflict ? router.push(SIGN_IN_LINK) : setShowErrorModal(false);
-        }}
-        closeModal={() => {
-          setShowErrorModal(false);
-        }}
-      />
+      ) : (
+        { showContent } && (
+          <>
+            <div className="px-6 pb-28 pt-10 md:px-24">
+              <NavTitle
+                title={APPLICANT_FORM_TEXT.HEADER}
+                navawayText={'Already have an account? '}
+                navLink={SIGN_IN_LINK}
+                navText={'Sign in'}
+              />
+              <div className="m-auto mt-8 max-w-[344px] md:mt-10 lg:mt-8">
+                {/* New user form */}
+                <ApplicantSignupForm
+                  showUserExistsError={isConflict}
+                  isAuthenticated={isAuthenticated}
+                  user={user}
+                  handleSubmit={handleSubmit}
+                  setShowPrivacyModal={setShowPrivacyModal}
+                  isTurnstileValid={isTurnstileValid}
+                  setIsTurnstileValid={setIsTurnstileValid}
+                />
+              </div>
+              {/* Navaway for organizations */}
+              <div className="mt-6 text-center">
+                {APPLICANT_FORM_TEXT.IFORG[0]}
+                <span className="text-blue-1 underline underline-offset-4">
+                  <Link href={ORG_SIGNUP_LINK}>
+                    {APPLICANT_FORM_TEXT.IFORG[1]}
+                  </Link>
+                </span>
+              </div>
+            </div>
+            <TableModal
+              tableData={APPLICANT_CONTENT_TABLE_TEXT}
+              headerText={PRIVACY_MODAL_TEXT.HEADER}
+              bodyText={PRIVACY_MODAL_TEXT.BODY}
+              extras={privacyModalExtras}
+              isOpen={showPrivacyModal}
+              closeModal={() => {
+                setShowPrivacyModal(false);
+              }}
+            />
+            <ErrorModal
+              isOpen={showErrorModal}
+              titleText={ERROR_MODAL_TEXT.requestFailed}
+              descriptionText={ERROR_MODAL_TEXT.somethingWrong}
+              buttonText={ERROR_MODAL_TEXT.okButton}
+              buttonHandler={() => {
+                isConflict
+                  ? router.push(SIGN_IN_LINK)
+                  : setShowErrorModal(false);
+              }}
+              closeModal={() => {
+                setShowErrorModal(false);
+              }}
+            />
+          </>
+        )
+      )}
     </div>
   );
 };
