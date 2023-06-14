2516566c2a0cb039127701a43850eabaa91ad9db
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-06-14 12:17:01 -0400

    fix: remove unnecessary component

--- src/modules/components/input/freeTextOther/BaseTemplate.stories.tsx
+++
@@ -1,8 +0,0 @@
-import BaseTemplate from './freeTextOther';
-import { mockBaseTemplateProps } from './freeTextOther.mocks';
-
-export default { component: BaseTemplate };
-
-export const Default = {
-  args: { ...mockBaseTemplateProps.base },
-};
--- src/modules/components/input/freeTextOther/freeTextOther.mocks.ts
+++
@@ -1,9 +0,0 @@
-import { IBaseTemplate } from './freeTextOther';
-
-const base: IBaseTemplate = {
-  sampleTextProp: 'Hello world!',
-};
-
-export const mockBaseTemplateProps = {
-  base,
-};
