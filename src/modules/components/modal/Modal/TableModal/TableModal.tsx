9593ccc6ddfd4c71a388f537c1386807c632b63f
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-07-10 10:32:35 -0400

    fix: modal display tweaks

--- src/modules/components/modal/Modal/TableModal/TableModal.tsx
+++ src/modules/components/modal/Modal/TableModal/TableModal.tsx
@@ -36,7 +36,7 @@ const TableModal: React.FC<ITableModal> = ({
             !showTableModal ? 'hidden' : ''
           }`}
         />
-        <div className="fixed bottom-0 left-[50%] right-4 top-[50%] z-50 mx-auto h-[calc(100%-20px)] max-h-[680px] w-[calc(100%-20px)] max-w-[722px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white">
+        <div className="fixed bottom-0 left-[50%] right-[50%] top-[50%] z-50 mx-auto h-[calc(100%-20px)] max-h-[680px] w-[calc(100%-20px)] max-w-[722px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white">
           <Dialog.Panel>
             <XMarkIcon
               className="absolute right-2 top-4 z-50 h-6 w-6 cursor-pointer stroke-2 text-black-text md:right-7 md:top-7"
