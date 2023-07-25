34e6e6310166060d8474168b6652e757bef75eec
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-07-25 18:16:17 -0400

    test: example unit test

---
+++ cypress/component/lib/helpers/formHelpers/printErrorMessages.cy.tsx
@@ -0,0 +1,11 @@
+describe('printErrorMessages', () => {
+  //   const inputid = 'test-id';
+  //   it('should display max length error before submit', () => {
+  //     const errors = ['String must contain at most 500 character(s)'];
+  //     // TODO: Change to expected error
+  //     const expectedErrMsg = 'test';
+  //     const expected = printErrorMessages(inputid, false, errors);
+  //     cy.mount(expected);
+  //     cy.get(`p#errorMessage-${inputid}`).should('have.text', expectedErrMsg);
+  //   });
+});
