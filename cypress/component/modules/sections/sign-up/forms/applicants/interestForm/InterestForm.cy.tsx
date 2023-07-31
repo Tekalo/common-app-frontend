95f75ee556999d035dbd3920b53ed9a3b9f5da4f
Seth <106334996+sgoodluck@users.noreply.github.com>
2023-07-31 13:35:15 -0400

    test: finishes unit tests

--- cypress/component/modules/sections/sign-up/forms/applicants/interestForm/InterestForm.cy.tsx
+++ cypress/component/modules/sections/sign-up/forms/applicants/interestForm/InterestForm.cy.tsx
@@ -64,14 +64,68 @@ describe('Applicant <InterestForm />', () => {
 
     it('renders all fields including work arrangement when part time selected and not referenceAttributionOther', () => {
       cy.mountInterestForm(props);
-      // TODO
+      // TODO select part time
+
       // Fields
+      cy.get(Selectors.employmentType.fullTime).should('exist');
+      cy.get(Selectors.employmentType.partTime).should('exist').click();
+
+      // Only rendered if part time
+      cy.get(Selectors.workArrangement.input).should('exist');
+
+      cy.get(Selectors.hoursPerWeek.input).should('exist');
+      cy.get(Selectors.roles.input).should('exist');
+      cy.get(Selectors.location.input).should('exist');
+      cy.get(Selectors.relocation.input).should('exist');
+      cy.get(Selectors.remote.input).should('exist');
+      cy.get(Selectors.salary.input).should('exist');
+      cy.get(Selectors.interestCauses.input).should('exist');
+      cy.get(Selectors.otherCauses.input).should('exist');
+      cy.get(Selectors.workAuthorization.input).should('exist');
+      cy.get(Selectors.govInterest.no).should('exist');
+      cy.get(Selectors.govInterest.yes).should('exist');
+      cy.get(Selectors.govInterestType.input).should('exist');
+      cy.get(Selectors.previousExperience.no).should('exist');
+      cy.get(Selectors.previousExperience.yes).should('exist');
+      cy.get(Selectors.essay.input).should('exist');
+      cy.get(Selectors.referenceAttribution.input).should('exist');
+      cy.get(Selectors.referenceAttributionOther.input).should('not.exist');
     });
 
     it('renders all fields including work arrangement when part time selected and referenceAttributionOther when referenceAttribution is "Other"', () => {
       cy.mountInterestForm(props);
       // TODO
       // Fields
+      cy.get(Selectors.employmentType.fullTime).should('exist');
+      cy.get(Selectors.employmentType.partTime).should('exist');
+
+      // Only rendered if part time
+      cy.get(Selectors.workArrangement.input).should('not.exist');
+
+      cy.get(Selectors.hoursPerWeek.input).should('exist');
+      cy.get(Selectors.roles.input).should('exist');
+      cy.get(Selectors.location.input).should('exist');
+      cy.get(Selectors.relocation.input).should('exist');
+      cy.get(Selectors.remote.input).should('exist');
+      cy.get(Selectors.salary.input).should('exist');
+      cy.get(Selectors.interestCauses.input).should('exist');
+      cy.get(Selectors.otherCauses.input).should('exist');
+      cy.get(Selectors.workAuthorization.input).should('exist');
+      cy.get(Selectors.govInterest.no).should('exist');
+      cy.get(Selectors.govInterest.yes).should('exist');
+      cy.get(Selectors.govInterestType.input).should('exist');
+      cy.get(Selectors.previousExperience.no).should('exist');
+      cy.get(Selectors.previousExperience.yes).should('exist');
+      cy.get(Selectors.essay.input).should('exist');
+      cy.get(Selectors.referenceAttribution.input).should('exist');
+      cy.get(Selectors.referenceAttributionOther.input).should('not.exist');
+
+      const referenceAttribution = cy.get(Selectors.referenceAttribution.input);
+      referenceAttribution.click();
+      cy.wait(1000); // wait for animation to finish
+      cy.get(Selectors.referenceAttribution.options.other).click();
+      cy.wait(1000); // wait for animation to finish
+      cy.get(Selectors.referenceAttributionOther.input).should('exist');
     });
   });
 });
