95d106374aabf9b1c66b291eac4deae3574834e9
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-09-01 16:53:11 -0400

    test: cleanup

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,9 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
 import './commands';
 import './common';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
