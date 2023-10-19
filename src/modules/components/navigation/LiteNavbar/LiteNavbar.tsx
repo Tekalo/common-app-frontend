16303d30cde79bdad8cb8a4e301fb20e1c2fa99f
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-10-19 16:07:56 -0400

    fix: put padding back

--- src/modules/components/navigation/LiteNavbar/LiteNavbar.tsx
+++ src/modules/components/navigation/LiteNavbar/LiteNavbar.tsx
@@ -79,7 +79,7 @@ const LiteNavbar: React.FC<ILiteNavbar> = ({
                           <UserCircleIcon className="w-6" />
                         ) as unknown as IconType
                       }
-                      className="flex cursor-pointer items-center justify-center space-x-1 px-1 py-2 font-sans text-component-large md:px-6 md:py-3"
+                      className="flex cursor-pointer items-center justify-center space-x-1 px-2 py-2 font-sans text-component-large md:px-6 md:py-3"
                     >
                       {NAV_BAR_TEXT.MY_ACCOUNT}
                     </Button>
