ede0367e15d4926a46088bae1dadd16c5b110f2a
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-10-19 13:37:20 -0400

    feat: 404 and 500 page wrapper

---
+++ src/modules/components/pages/wrappers/errorPageWrapper.tsx
@@ -0,0 +1,28 @@
+import Button, { ButtonVariant } from '@/components//buttons/Button/Button';
+import { BASE_LINK } from '@/lang/en';
+import router from 'next/router';
+
+interface IErrorPageWrapper {
+  headline: string;
+  bodyText: string;
+}
+
+const ErrorPageWrapper: React.FC<IErrorPageWrapper> = ({
+  headline,
+  bodyText,
+}) => {
+  return (
+    <div className="m-auto max-w-[390px] px-6 pb-32 pt-20 text-center md:max-w-[224px] md:px-0 md:pt-24 lg:max-w-[544px]">
+      <h2 className="mb-6 font-display text-h2-mobile">{headline}</h2>
+      <div className="text-p1-mobile">{bodyText}</div>
+      <Button
+        className="m-auto mt-8 w-full px-7 lg:w-auto"
+        variant={ButtonVariant.OUTLINED}
+        label="Back to home"
+        onClick={() => router.push(BASE_LINK)}
+      />
+    </div>
+  );
+};
+
+export default ErrorPageWrapper;
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,25 +1,14 @@
-import Button, { ButtonVariant } from '@/components/buttons/Button/Button';
-import { BASE_LINK, ERROR_TEXT } from '@/lang/en';
+import { ERROR_TEXT } from '@/lang/en';
 import ApplicationLayout from '@/lib/layouts/application/ApplicationLayout';
 import { NextPageWithLayout } from '@/lib/types';
-import { useRouter } from 'next/router';
+import ErrorPageWrapper from '@/modules/components/pages/wrappers/errorPageWrapper';
 
 const Page500: NextPageWithLayout = () => {
-  const router = useRouter();
-
   return (
-    <div className="m-auto max-w-[390px] px-6 pb-32 pt-20 text-center md:max-w-[224px] md:px-0 md:pt-24 lg:max-w-[544px]">
-      <h2 className="mb-6 font-display text-h2-mobile">
-        {ERROR_TEXT.unhandledCode}
-      </h2>
-      <div className="text-p1-mobile">{ERROR_TEXT.unhandledText}</div>
-      <Button
-        className="m-auto mt-8 w-full px-7 lg:w-auto"
-        variant={ButtonVariant.OUTLINED}
-        label="Back to home"
-        onClick={() => router.push(BASE_LINK)}
-      />
-    </div>
+    <ErrorPageWrapper
+      headline={ERROR_TEXT.unhandledCode}
+      bodyText={ERROR_TEXT.unhandledText}
+    />
   );
 };
 
