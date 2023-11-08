3182271c36590249c70f32ba1134985ce423c0aa
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-11-08 14:39:30 -0500

    feat: most content updates, refactor sub components

---
+++ src/modules/sections/home/aboutSection/components/pronunciationBox.tsx
@@ -0,0 +1,27 @@
+import { HOME_ABOUT_TEXT } from '@/lang/en';
+
+const PronunciationBox: React.FC = () => (
+  <div className="relative mx-auto max-w-[445px]">
+    {/* Main content bg */}
+    <div className="absolute bottom-0 left-0 right-1 top-0 z-20 rounded border border-black-text bg-light-orange"></div>
+    {/* Drop-shadow box */}
+    <div className="absolute -bottom-1 left-1 right-0 top-1 z-10 rounded border border-black-text bg-illustration-orange"></div>
+
+    {/* Text container */}
+    <div className="m-auto mb-16 p-5 lg:mb-20 lg:max-w-[544px]">
+      <div className="relative z-30 space-y-3 text-p2-mobile md:space-y-6 lg:text-p2-desktop">
+        <div>
+          <span className="font-display font-semibold">
+            {HOME_ABOUT_TEXT.PRONUNCIATION.TE}
+          </span>
+          {HOME_ABOUT_TEXT.PRONUNCIATION.KALO}
+          <br />
+          {HOME_ABOUT_TEXT.PRONUNCIATION.DEFINITION}
+        </div>
+        <div>{HOME_ABOUT_TEXT.PRONUNCIATION.GOAL}</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default PronunciationBox;
