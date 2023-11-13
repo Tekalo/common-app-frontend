3c9157b7d08e554c0b9402b0c1e99e0cbcb8f47e
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-11-13 09:40:36 -0500

    feat: no extra dash

--- cypress/e2e/candidate-application.cy.ts
+++ cypress/e2e/candidate-application.cy.ts
@@ -361,7 +361,7 @@ describe('Candidate Application', () => {
   }
 
   function fillEmail(): void {
-    const randomEmail = `success+test-user-${new Date().getTime()}@simulator.amazonses.com`;
+    const randomEmail = `success+test-user${new Date().getTime()}@simulator.amazonses.com`;
     cy.get(Selectors.email.input).fastType(randomEmail);
   }
 
