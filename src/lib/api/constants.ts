d90005386a8b4779b3fa03dc6fbe85fbadd9745d
Taylor Hawkins <thawkins@schmidtfutures.com>
2023-12-19 12:40:48 -0500

    feat: refactor bff

---
+++ src/lib/api/constants.ts
@@ -0,0 +1,4 @@
+export const debugHeaderName = 'X-Debug';
+export const turnstileTokenHeaderName = 'x-turnstile-token';
+export const turnstileEndpoint =
+  'https://challenges.cloudflare.com/turnstile/v0/siteverify';
