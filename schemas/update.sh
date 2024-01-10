f83ef1cd733b677cfaf2b8fe5a55ab76555b7b98
Taylor Hawkins <thawkins@schmidtfutures.com>
2024-01-10 11:25:00 -0500

    test: try this

--- schemas/update.sh
+++ schemas/update.sh
@@ -10,5 +10,10 @@ cp -f ${apiDir}/applicants.ts ${localDir}/applicants.ts
 cp -f ${apiDir}/opportunities.ts ${localDir}/opportunities.ts
 cp -f ${apiDir}/shared.ts ${localDir}/shared.ts
 
-sed -i '' -e ${replaceReg} ${localDir}/applicants.ts
-sed -i '' -e ${replaceReg} ${localDir}/opportunities.ts
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e ${replaceReg} ${localDir}/applicants.ts
+  sed -i '' -e ${replaceReg} ${localDir}/opportunities.ts
+else
+  sed -i -e ${replaceReg} ${localDir}/applicants.ts
+  sed -i -e ${replaceReg} ${localDir}/opportunities.ts
+fi
