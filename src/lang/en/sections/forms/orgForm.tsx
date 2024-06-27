7e1bb33dd5fb5848bd5cb917899c79bdbf53139d
tshaneh <129786566+tshaneh@users.noreply.github.com>
2024-06-27 13:08:17 -0400

    CAPP-1482: Update calendly on tekalo.org (#727)

--- src/lang/en/sections/forms/orgForm.tsx
+++ src/lang/en/sections/forms/orgForm.tsx
@@ -21,7 +21,7 @@ export const ORG_FORM_TEXT = {
         <a
           className="whitespace-nowrap text-blue-1 underline"
           target="_blank"
-          href="https://calendly.com/eperakis-1/30min"
+          href="https://calendly.com/alexandra-alltechishuman"
           rel="noreferrer"
         >
           Schedule a chat
